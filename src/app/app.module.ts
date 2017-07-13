20962e4cfd30ef8fe75d7cc3104d5bb9ee7516fc
mariajesus.fernandez <mariajesus.fernandez@alfa9.com>
2017-07-13 09:08:07 +0200

    Agregando mas componentes de PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { DatatableExampleComponent } from './datatable-example/datatable-example.component';
-
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponentComponent } from './welcome-component/welcome-component.component';
-
 import { HttpModule } from '@angular/http';
-
-import {DataTableModule,SharedModule, GrowlModule,ContextMenuModule} from 'primeng/primeng';
+import { DataTableModule,SharedModule, GrowlModule,ContextMenuModule } from 'primeng/primeng';
 
 const aRutas : Routes = [
   { path: '', component: WelcomeComponentComponent },
